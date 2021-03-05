91275754d599e64a937a76319e792a0c74cc30cd
nicolast654 <nicolast654@gmail.com>
2021-03-05 22:57:11 +0100

    Added Pagerank

---
+++ pagerank/pagerank.py
@@ -0,0 +1,130 @@
+import os
+import random
+import re
+import sys
+
+DAMPING = 0.85
+SAMPLES = 10000
+
+
+def main():
+    if len(sys.argv) != 2:
+        sys.exit("Usage: python pagerank.py corpus")
+    corpus = crawl(sys.argv[1])
+    ranks = sample_pagerank(corpus, DAMPING, SAMPLES)
+    print(f"PageRank Results from Sampling (n = {SAMPLES})")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+    ranks = iterate_pagerank(corpus, DAMPING)
+    print(f"PageRank Results from Iteration")
+    for page in sorted(ranks):
+        print(f"  {page}: {ranks[page]:.4f}")
+
+def crawl(directory):
+    """
+    Parse a directory of HTML pages and check for links to other pages.
+    Return a dictionary where each key is a page, and values are
+    a list of all other pages in the corpus that are linked to by the page.
+    """
+    pages = dict()
+
+    # Extract all links from HTML files
+    for filename in os.listdir(directory):
+        if not filename.endswith(".html"):
+            continue
+        with open(os.path.join(directory, filename)) as f:
+            contents = f.read()
+            links = re.findall(r"<a\s+(?:[^>]*?)href=\"([^\"]*)\"", contents)
+            pages[filename] = set(links) - {filename}
+
+    # Only include links to other pages in the corpus
+    for filename in pages:
+        pages[filename] = set(
+            link for link in pages[filename]
+            if link in pages
+        )
+
+    return pages
+
+
+def transition_model(corpus, page, damping_factor):
+    """
+    Return a probability distribution over which page to visit next,
+    given a current page.
+
+    With probability `damping_factor`, choose a link at random
+    linked to by `page`. With probability `1 - damping_factor`, choose
+    a link at random chosen from all pages in the corpus.
+    """
+    transition_model = {key:0.0 for key in corpus.keys()}
+
+    initial = (1-damping_factor) / len(corpus)
+    for key in transition_model:
+        if key in corpus[page]:
+            transition_model[key] = initial+damping_factor/len(corpus[page])
+        else:
+            transition_model[key] = initial
+
+    return transition_model
+
+
+def sample_pagerank(corpus, damping_factor, n):
+    """
+    Return PageRank values for each page by sampling `n` pages
+    according to transition model, starting with a page at random.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    PR = {key:0.0 for key in corpus.keys()}
+
+    current = random.choice(list(corpus))
+    PR[current] += (1 / n)
+
+    for i in range(n):
+        model = transition_model(corpus, current, damping_factor)
+        current = random.choices(list(model.keys()), weights=list(model.values()), k=1)[0]
+        PR[current] += (1 / n)
+
+    return PR
+
+
+def iterate_pagerank(corpus, damping_factor):
+    """
+    Return PageRank values for each page by iteratively updating
+    PageRank values until convergence.
+
+    Return a dictionary where keys are page names, and values are
+    their estimated PageRank value (a value between 0 and 1). All
+    PageRank values should sum to 1.
+    """
+    start = dict()
+    for page in corpus.keys():
+        start[page] = 1/len(corpus)
+
+
+    run = True
+    while run:
+        PR = dict()
+        for p in corpus.keys():
+            PR_p = (1-damping_factor)/len(corpus)
+            for page, links in corpus.items():
+                if len(links) != 0:
+                    if page!=p and p in links:
+                        PR_p += damping_factor*(start[page]/len(corpus[page]))
+                else:
+                    PR_p += damping_factor*(start[page]/len(corpus))
+            PR[p] = PR_p
+
+        for page in PR.keys():
+            if round(PR[page]-start[page], 3)==0:
+                run=False
+
+        start = PR
+
+    return PR 
+
+
+if __name__ == "__main__":
+    main()
